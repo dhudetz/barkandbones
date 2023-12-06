e12bee877f58a96abe1d2a6362d8edbf613c886e
Danny Hudetz <dannyhudetz@gmail.com>
2023-12-05 21:35:08 -0600

    Python runs on docker container

--- backend/app.py
+++ backend/app.py
@@ -10,4 +10,4 @@ def hello_world():
     return jsonify({"message": "Hello from the backend!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5001)
