7cbd5e435e02361c25e746e4dec989fa4667bb61
Danny Hudetz <dannyhudetz@gmail.com>
2023-12-07 15:38:02 -0600

    Website UI Prototype
    (+) Home page, about page, order page with switching toolbar and animations

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,43 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    funtime();
-    setupEventSource();
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const pages = document.querySelectorAll('.page');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            // Remove active class from all links and pages
+            navLinks.forEach(link => link.classList.remove('active'));
+            pages.forEach(page => page.classList.remove('active'));
+
+            // Add active class to clicked link and corresponding page
+            this.classList.add('active');
+            const pageId = this.dataset.page;
+            document.getElementById(pageId).classList.add('active');
+        });
+    });
 });
-
-function funtime() {
-    const h1 = document.querySelector('h1');
-    const img = document.querySelector('img');
-
-    const minWidth = 250; // Minimum width
-    const maxWidth = 300; // Maximum width
-    const amplitude = (maxWidth - minWidth) / 2; // Amplitude of the sine wave
-    const midWidth = minWidth + amplitude; // Middle width value
-    let angle = 0; // Angle for the sine wave
-
-    setInterval(() => {
-        // Generate a random color
-        const randomColor = '#' + ('000000' + Math.floor(Math.random() * 16777215).toString(16)).slice(-6);
-        h1.style.color = randomColor;
-    }, 200);
-
-    setInterval(() => {
-        // Calculate the new width using sine wave
-        img.style.width = (midWidth + amplitude * Math.sin(angle)) + "px";
-
-        angle += 0.05; // Increment the angle for the next frame
-    }, 10);
-}
-
-function setupEventSource() {
-    const h1 = document.querySelector('h1');
-    const eventSource = new EventSource(`${config.API_BASE_URL}/api`);
-
-    eventSource.onmessage = (event) => {
-        const parsedData = JSON.parse(event.data);
-        h1.textContent += parsedData.message;
-    };
-
-    eventSource.onerror = (error) => {
-        console.error('EventSource failed:', error);
-        eventSource.close();
-    };
-}
