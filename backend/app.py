36c72395e9241758f86692d29432e561bf77c3f5
Danny Hudetz <dannyhudetz@gmail.com>
2023-12-08 15:58:57 -0600

    Accept and deny links

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,28 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
 from threading import Lock
 from twilio.rest import Client
+from random import randint
+import smtplib  # For sending email notifications
 
 app = Flask(__name__)
 CORS(app)
 
-# Initialize a lock
+# Initialize locks
 processing_lock = Lock()
 confirm_lock = Lock()
 deny_lock = Lock()
 
+# Dictionary to store order numbers and associated email addresses
+order_email_dict = {}
+
 def set_backend_directory():
-    # Get the current working directory
     current_dir = os.getcwd()
-
-    # Get the name of the last folder in the current path
     folder_name = os.path.basename(current_dir)
-
-    # Check if the current folder is 'barkandbones'
     if folder_name == 'barkandbones':
-        # Change directory to './backend'
-        new_dir = os.path.join(current_dir, 'backend')
-        os.chdir(new_dir)
-        print(f"Changed directory to {new_dir}")
-    elif folder_name == 'backend':
-        print("Already in 'backend' directory")
-    else:
+        os.chdir(os.path.join(current_dir, 'backend'))
+    elif folder_name != 'backend':
         print(f"Current directory is '{folder_name}', not changing directory")
 
 def get_api_key(filepath):
@@ -37,77 +32,75 @@ def get_api_key(filepath):
 set_backend_directory()
 api_key_path = 'api_key.txt'
 auth_token = get_api_key(api_key_path)
-print(auth_token)
 account_sid = 'ACcb8aab937cd40cdb342c3e8246b96b35'
 client = Client(account_sid, auth_token)
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/order-deny', methods=['POST'])
-def deny_order():
-    with confirm_lock:
-        order_data = request.get_json()
-        process_order(order_data)
-        return jsonify({"message": "Order received successfully"}), 200
+def send_email(recipient_email, subject, body):
+    # Configure your email server and sender email here
+    sender_email = "your_email@example.com"
+    password = "your_password"
+    server = smtplib.SMTP('smtp.example.com', 587)
+    server.starttls()
+    server.login(sender_email, password)
+    message = f'Subject: {subject}\n\n{body}'
+    server.sendmail(sender_email, recipient_email, message)
+    server.quit()
 
 @app.route('/api/order-confirm', methods=['POST'])
 def confirm_order():
+    order_id = request.json['order_id']
+    with confirm_lock:
+        email = order_email_dict.get(order_id)
+        if email:
+            send_email(email, "Order Confirmation", f"Your order {order_id} has been confirmed.")
+            return jsonify({"message": "Order confirmed"}), 200
+        else:
+            return jsonify({"error": "Order ID not found"}), 404
+
+@app.route('/api/order-deny', methods=['POST'])
+def deny_order():
+    order_id = request.json['order_id']
     with deny_lock:
-        order_data = request.get_json()
-        process_order(order_data)
-        return jsonify({"message": "Order received successfully"}), 200
+        email = order_email_dict.get(order_id)
+        if email:
+            send_email(email, "Order Denied", f"Your order {order_id} has been denied.")
+            return jsonify({"message": "Order denied"}), 200
+        else:
+            return jsonify({"error": "Order ID not found"}), 404
 
 @app.route('/api/order', methods=['POST'])
 def receive_order():
     with processing_lock:
         order_data = request.get_json()
-        process_order(order_data)
-        return jsonify({"message": "Order received successfully"}), 200
+        order_id = process_order(order_data)
+        return jsonify({"message": "Order received successfully", "order_id": order_id}), 200
 
-def process_order(order_data):
-    text_message = generate_order_message(order_data)
-    phone_number = '+17084463055'
-    send_info_text(text_message, phone_number)
-
-def send_info_text(message, number):
+def send_info_text(text_message, phone_number):
     client.messages.create(
         from_ = '+18552044131',
-        body = message,
-        to = number
+        body = text_message,
+        to = phone_number
     )
 
-def generate_order_message(order_data):
-    # Group items by name and count the quantities
-    item_counts = {}
-    total_cost = 0
-    for item in order_data['orderItems']:
-        if item['name'] not in item_counts:
-            item_counts[item['name']] = 0
-        item_counts[item['name']] += 1
-        total_cost += item['price']
-
-    # Construct the items string
+def generate_order_message(order_data, order_id):
+    item_counts = {item['name']: order_data['orderItems'].count(item) for item in order_data['orderItems']}
+    total_cost = sum(item['price'] for item in order_data['orderItems'])
     items_string = "\n".join(f"{count}x {name}" for name, count in item_counts.items())
-
-    # Construct the message
     message = (
-    "NEW ORDER!\n\n"
-        f"{order_data['customerName']}\n"
-        f"{order_data['phoneNumber']}\n"
-        f"{order_data['email']}\n\n"
-        f"{items_string}\n\n"
-        f"Total: ${total_cost:.2f}\n\n"
-        f"Message: {order_data['specialInstructions']}\n\n"
-        "<hyperlink>Accept Order\n\n"
-        "<hyperlink>Reject Order"
+        f"NEW ORDER!\n\n{order_data['customerName']}\n{order_data['phoneNumber']}\n{order_data['email']}\n\n"
+        f"{items_string}\n\nTotal: ${total_cost:.2f}\n\nMessage: {order_data['specialInstructions']}\n\n"
+        f"Confirm Order Link: http://barkandbones.org/api/order-confirm/{order_id}\nDeny Order Link: http://barkandbones.org/api/order-deny/{order_id}\n"
+        f"Order Number: {order_id}"
     )
-
-    # For demonstration purposes, we just print the message
-    print(message)
-
-    # Return the message in case you need it as a return value
     return message
+
+def process_order(order_data):
+    order_id = randint(1000000000, 9999999999)
+    order_email_dict[order_id] = order_data['email']
+    text_message = generate_order_message(order_data, order_id)
+    phone_number = '+17084463055'
+    send_info_text(text_message, phone_number)
+    return order_id
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5001)
