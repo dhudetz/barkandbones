84602215f6be57ee52eb9305bd0d2995ba8b801c
Danny Hudetz <dannyhudetz@gmail.com>
2023-12-09 02:50:28 -0600

    Tweak

--- frontend/src/order_handler.js
+++ frontend/src/order_handler.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    rainBones();
-
     const addToCartButton = document.getElementById('add-to-cart');
     const clearCartButton = document.getElementById('clear-cart');
     const cartItemsList = document.getElementById('cart-items');
