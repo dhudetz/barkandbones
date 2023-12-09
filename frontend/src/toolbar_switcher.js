cd4b2e8796f957039af71955beb6bf6eb37bde1f
Danny Hudetz <dannyhudetz@gmail.com>
2023-12-09 03:08:50 -0600

    Scroll to top

--- frontend/src/toolbar_switcher.js
+++ frontend/src/toolbar_switcher.js
@@ -13,11 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // Remove active class from all links and pages
         navLinks.forEach(link => link.classList.remove('active'));
         pages.forEach(page => page.classList.remove('active'));
-
+    
         // Find the link and page corresponding to pageName
         const activeLink = [...navLinks].find(link => link.dataset.page === pageName);
         const activePage = document.getElementById(pageName);
-
+    
         // Add active class to them
         if (activeLink) {
             activeLink.classList.add('active');
@@ -25,7 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (activePage) {
             activePage.classList.add('active');
         }
+    
+        // Scroll to the top of the page
+        window.scrollTo(0, 0);
     }
-
-    window.setActivePage = setActivePage;
+    
+    window.setActivePage = setActivePage;    
 });
